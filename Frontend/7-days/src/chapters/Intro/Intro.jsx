b5a57e69143f58af2c26a30cef90f16641b59207
joodsee8 <joodsee8@gmail.com>
2026-07-22 18:53:30 -0600

    Actualizar Intro.jsx

---
+++ Frontend/7-days/src/chapters/Intro/Intro.jsx
@@ -0,0 +1,19 @@
+import "./Intro.css";
+
+function Intro() {
+    return (
+        <main className="intro">
+
+            <div className="typewriter">
+
+                <p className="cursor">
+                    |
+                </p>
+
+            </div>
+
+        </main>
+    );
+}
+
+export default Intro;
