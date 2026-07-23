e7d0be1700438c924b2d5a513780b22500b189ef
joodsee8 <joodsee8@gmail.com>
2026-07-22 19:30:09 -0600

    Actualizar Intro.jsx

--- Frontend/7-days/src/chapters/Intro/Intro.jsx
+++ Frontend/7-days/src/chapters/Intro/Intro.jsx
@@ -2,36 +2,29 @@ import "./Intro.css";
 import Typewriter from "../../components/Typewriter/Typewriter";
 
 function Intro() {
+  const script = [
+    {
+      text: "Hola, Montse.",
+      speed: 75,
+      pause: 2500,
+    },
+    {
+      text: "Quiero pedirte siete días.",
+      speed: 75,
+      pause: 3500,
+    },
+    {
+      text: "Nada de esto necesita una respuesta.",
+      speed: 70,
+      pause: 2500,
+    },
+  ];
 
-    const script = [
-
-        {
-            text: "Hola, Montse.",
-            pause: 2500
-        },
-
-        {
-            text: "Quiero pedirte siete días.",
-            pause: 3000
-        },
-
-        {
-            text: "Nada de esto necesita una respuesta.",
-            pause: 2500
-        }
-
-    ];
-
-    return (
-
-        <main className="intro">
-
-            <Typewriter script={script}/>
-
-        </main>
-
-    );
-
+  return (
+    <main className="intro">
+      <Typewriter script={script} />
+    </main>
+  );
 }
 
 export default Intro;
