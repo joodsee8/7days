67b85e37d2d97dbeaf0c7ee691a63215a93f2284
joodsee8 <joodsee8@gmail.com>
2026-08-01 22:05:16 -0600

    Actualizar Ribs.jsx

--- frontend/src/chapters/Day2/Ribs.jsx
+++ frontend/src/chapters/Day2/Ribs.jsx
@@ -168,7 +168,7 @@ const Ribs = () => {
       {showPolaroid && (
         <div className="polaroids-section fade-in-chapter">
           <Polaroid 
-            imageSrc="/assets/images/Polaroid2.jpg" 
+            imageSrc={polaroidImg}
             message="Montse es la persona que pase lo que pase estará para ti, escuchándote, apoyándote y abrazándote, que haría cualquier cosa por ti, su amistad es de las pocas que quedan sin envidias e hipocresías." 
             friendName="Pao" 
           />
