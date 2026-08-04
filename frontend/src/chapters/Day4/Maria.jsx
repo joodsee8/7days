9f690395386e6ff4a89093c64f82aadcad011aa8
joodsee8 <joodsee8@gmail.com>
2026-08-03 23:34:24 -0600

    Actualizar Maria.jsx

--- frontend/src/chapters/Day4/Maria.jsx
+++ frontend/src/chapters/Day4/Maria.jsx
@@ -3,7 +3,7 @@ import './Maria.css';
 import MusicPlayer from '../../components/MusicPlayer/MusicPlayer'; // Importamos el reproductor
 import coverImg from '../../assets/images/pajarito.jpg'
 import audioFile from '../../assets/music/Maria.mp3'
-import polaroidImg from '../../assets/images/Polaroid.jpg'
+import polaroidImg from '../../assets/images/Polaroid1.jpg'
 import polaroidImg2 from '../../assets/images/Polaroid2.jpg'
 
 const letterParagraphs = [
