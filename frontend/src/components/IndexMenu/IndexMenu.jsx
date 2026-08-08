a3628dc984a78ebc52accb911b0cc25a0575da4d
joodsee8 <joodsee8@gmail.com>
2026-08-08 00:13:01 -0600

    Actualizar IndexMenu.jsx

--- frontend/src/components/IndexMenu/IndexMenu.jsx
+++ frontend/src/components/IndexMenu/IndexMenu.jsx
@@ -24,7 +24,7 @@ const IndexMenu = () => {
     { id: 3, chapter: "Capítulo III", title: "Seven", path: "/day3", unlocked: checkUnlocked(3) },
     { id: 4, chapter: "Capítulo IV", title: "María la curandera", path: "/day4", unlocked: checkUnlocked(4) },
     { id: 5, chapter: "Capítulo V", title: "Pajarito colibrí", path: "/day5", unlocked: checkUnlocked(6) },
-    { id: 6, chapter: "Capítulo VI", title: "Long Live", path: "/day6", unlocked: checkUnlocked(8) },
+    { id: 6, chapter: "Capítulo VI", title: "Long Live", path: "/day6", unlocked: checkUnlocked(30) },
     { id: 7, chapter: "Capítulo VII", title: "Mary", path: "/day7", unlocked: checkUnlocked(8) },
   ];
 
