210b2f6b71f8b3ddbd6a9684bb6644e77a6b19a7
joodsee8 <joodsee8@gmail.com>
2026-09-16 18:36:05 -0600

    Actualizar Long.jsx

--- frontend/src/chapters/Day6/Long.jsx
+++ frontend/src/chapters/Day6/Long.jsx
@@ -13,7 +13,7 @@ import LockedCard from '../../components/LockedCard/LockedCard';
 
 const CHAPTER_INDEX = 6;
 const CHAPTER_TOTAL = 7;
-const ACCENT = '#4A14BC';
+const ACCENT = '#4F3485';
 const PLAYER_BG = '#000000';
 const PLAYER_TEXT = '#E5E5E5';
 const LETTER_SEEN_KEY = 'day6_letter_typed';
